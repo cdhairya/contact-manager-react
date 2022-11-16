dfa3d72f2ee93bb389fc82e144c5380a54778a43
Dhairya Chheda <dhairyakchheda@gmail.com>
2022-11-16 22:37:47 +0530

    Contact and Header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import "./App.css";
+import React, { Component } from "react";
+import Contact from "./components/Contact";
+import Header from "./components/Header";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>The App Component</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header branding="Contact Manager" />
+        <Contact name="John Doe" email="jdoe@email.com" phone="555-555-5555" />
+        <Contact
+          name="Karen Smith"
+          email="karen@email.com"
+          phone="333-333-3333"
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
