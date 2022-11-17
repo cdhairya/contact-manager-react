a861e768a5a94c6db8bbce97c094d6d4086a9156
Dhairya Chheda <dhairyakchheda@gmail.com>
2022-11-17 11:57:38 +0530

    Icon to show and hide contact info

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,20 +1,28 @@
-import React, { Component } from "react";
 import PropTypes from "prop-types";
+import { useState } from "react";
 
-class Contact extends Component {
-  render() {
-    const { name, email, phone } = this.props.contact;
-    return (
-      <div className="card card-body mb-3">
-        <h4>{name}</h4>
+const Contact = ({ contact }) => {
+  const { name, email, phone } = contact;
+
+  const [showContact, setShowContact] = useState(true);
+
+  const onShowClick = () => {
+    setShowContact(!showContact);
+  };
+  return (
+    <div className="card card-body mb-3">
+      <h4>
+        {name} <i onClick={onShowClick} className="fa-solid fa-sort-down"></i>
+      </h4>
+      {showContact && (
         <ul className="list-group">
           <li className="list-group-item">{email}</li>
           <li className="list-group-item">{phone}</li>
         </ul>
-      </div>
-    );
-  }
-}
+      )}
+    </div>
+  );
+};
 
 Contact.propTypes = {
   contact: PropTypes.object.isRequired,
