d8a582fd43defada5319621d7454b22c4fe55a01
Dhairya Chheda <dhairyakchheda@gmail.com>
2022-11-29 11:13:57 +0530

    Delete contact using context

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,34 +2,45 @@ import PropTypes from "prop-types";
 import { useState } from "react";
 import React from "react";
 
-const Contact = ({ contact, deleteClickHandler }) => {
-  const { name, email, phone } = contact;
+import { Consumer } from "../context";
+
+const Contact = ({ contact }) => {
+  const { id, name, email, phone } = contact;
 
   const [showContact, setShowContact] = useState(true);
-
+  const deleteClickHandler = (id, dispatch) => {
+    dispatch({ type: "DELETE_CONTACT", payload: id });
+  };
   return (
-    <div className="card card-body mb-3">
-      <h4>
-        {name}{" "}
-        <i
-          onClick={() => {
-            setShowContact(!showContact);
-          }}
-          className="fa-solid fa-sort-down"
-        ></i>
-        <i
-          className="fas fa-times"
-          style={{ cursor: "pointer", float: "right", color: "red" }}
-          onClick={() => deleteClickHandler()}
-        ></i>
-      </h4>
-      {showContact && (
-        <ul className="list-group">
-          <li className="list-group-item">{email}</li>
-          <li className="list-group-item">{phone}</li>
-        </ul>
-      )}
-    </div>
+    <Consumer>
+      {(value) => {
+        const { dispatch } = value;
+        return (
+          <div className="card card-body mb-3">
+            <h4>
+              {name}{" "}
+              <i
+                onClick={() => {
+                  setShowContact(!showContact);
+                }}
+                className="fa-solid fa-sort-down"
+              ></i>
+              <i
+                className="fas fa-times"
+                style={{ cursor: "pointer", float: "right", color: "red" }}
+                onClick={() => deleteClickHandler(id, dispatch)}
+              ></i>
+            </h4>
+            {showContact && (
+              <ul className="list-group">
+                <li className="list-group-item">{email}</li>
+                <li className="list-group-item">{phone}</li>
+              </ul>
+            )}
+          </div>
+        );
+      }}
+    </Consumer>
   );
 };
 
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -5,13 +5,6 @@ import Contact from "./Contact";
 import { Consumer, Provider } from "../context";
 
 const Contacts = () => {
-  // const [contacts, setContacts] = useState();
-
-  const deleteContent = (id) => {
-    // setContacts(contacts.filter((contact) => contact.id !== id));
-    console.log("delete");
-  };
-
   return (
     <Consumer>
       {(value) => {
@@ -19,11 +12,7 @@ const Contacts = () => {
         return (
           <>
             {contacts.map((contact) => (
-              <Contact
-                key={contact.id}
-                contact={contact}
-                deleteClickHandler={() => deleteContent(contact.id)}
-              />
+              <Contact key={contact.id} contact={contact} />
             ))}
           </>
         );
